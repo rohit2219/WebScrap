e45feda0159f71aa653e440f9fdf8c183eb4c63a
rohit2219 <rohit_2219@rediffmail.com>
2018-01-19 08:56:12 -0500

    changing the old search module

--- app/components/search.old/search.component.ts
+++ app/components/search.old/search.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { SearchService } from '../services/SearchService.service';
+
+export interface IHash { [details: string] : string }
+ 
+@NgModule({
+  declarations: [
+    SearchComponent
+  ],
+  imports: [BrowserModule,FormsModule],
+  providers: [SearchComponent],
+  bootstrap: [SearchComponent],
+})
 
 @Component({
-  selector: 'app-search',
+  selector: 'search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
+
 export class SearchComponent implements OnInit {
+    users:any[];
+    user = {
+        name:'',
+        email:'',
+        phone:''
+	};
+   tempTicker:string;
+   servicea:string;
+   constructor(public SearchInst:SearchService)
+   {
+   console.log('Constructor for SearchComponent Ran');
+   this.SearchInst.getUsers().subscribe(users => { this.users = users }); 
+   }
+    
+   ngOnInit() {
+     this.servicea = ""; 
+   }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+   onSubmit(){
+	  var retTicker="";
+   	  console.log("tempTicker:",this.tempTicker);
+	  this.servicea = this.SearchInst.SubmitTicker(this.tempTicker);
+	  
+    }
 
 }
+
